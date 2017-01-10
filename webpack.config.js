34dc8650719c9f0d9ce02089a8d2a20bb211429d
Ion Darii <i_darii@yahoo.com>
2017-01-10 15:00:19 -0800

    Add main component

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,13 @@ module.exports = {
   },
   resolve: {
     root: __dirname,
+    modulesDirectories: [
+      'node_modules',
+      './app/components'
+    ],
     alias: {
       // Main: 'app/components/Main.jsx',
+      // TodoApp: 'app/components/TodoApp.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
